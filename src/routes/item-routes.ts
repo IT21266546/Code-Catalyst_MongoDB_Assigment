ced81f98542613b6372d5e1156551bdef019cd3d
Sanduni Sathsarani <99157473+IT21266546@users.noreply.github.com>
2024-01-21 00:21:27 +0530

    feat: update item routes

--- src/routes/item-routes.ts
+++ src/routes/item-routes.ts
@@ -1,13 +1,23 @@
+// Importing necessary modules and dependencies
 import express from "express";
-import {ItemController} from "../controllers/item-controller";
+import { ItemController } from "../controllers/item-controller";
 
+// Creating an Express router
 const router = express.Router()
 
+// Route to handle the creation of a new item
 router.post('/add-item', ItemController.createItem);
 
-// TODO: Move the all routes from the server.ts to this
+// TODO: Move the following routes from the server.ts to this router
+
+// Route to handle fetching all items
 router.get('/get-items', ItemController.getItems);
+
+// Route to handle updating an item by ID
 router.patch('/update-item/:id', ItemController.updateItems);
+
+// Route to handle deleting an item by ID
 router.delete('/delete-item/:id',  ItemController.deleteItems);
 
-export default router
+// Exporting the router to be used in other parts of the application
+export default router;
